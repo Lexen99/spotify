4fa5b99a9856c0a6ab0590211a40d9278f771495
Marcus Pousette <marcuspousette@gmail.com>
2023-03-23 17:05:14 +0100

    Lektion 20 - Player api calls

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,7 +1,82 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Box, Grid, Typography, Avatar } from '@mui/material';
+import { getAccessTokenFromStorage } from '../../utils/getAccessTokenFromStorage';
 
 const Player = ({ spotifyApi }) => {
+	const [localPlayer, setPlayer] = useState(null);
+	const [is_paused, setPaused] = useState(false);
+	const [current_track, setTrack] = useState(null);
+	const [device, setDevice] = useState(null);
+	const [duration, setDuration] = useState(null);
+	const [progress, setProgress] = useState(null);
+
+	useEffect(() => {
+		const token = getAccessTokenFromStorage();
+		const script = document.createElement('script');
+		script.src = 'https://sdk.scdn.co/spotify-player.js';
+		script.async = true;
+		document.body.appendChild(script);
+
+		window.onSpotifyWebPlaybackSDKReady = () => {
+			const player = new window.Spotify.Player({
+				name: 'Techover Playback',
+				getOAuthToken: (cb) => {
+					cb(token);
+				},
+				volume: 0.5
+			});
+
+			player.addListener('ready', ({ device_id }) => {
+				console.log('Ready with Device ID', { device_id, player });
+				setDevice(device_id);
+				setPlayer(player);
+			});
+
+			player.addListener('player_state_changed', (state) => {
+				if (!state || !state.track_window?.current_track) {
+					return;
+				}
+				console.log(state);
+				const duration_ms = state.track_window.current_track.duration_ms / 1000;
+				const position_ms = state.position / 1000;
+				setDuration(duration_ms);
+				setProgress(position_ms);
+				setTrack(state.track_window.current_track);
+				setPaused(state.paused);
+			});
+
+			setPlayer(player);
+			player.connect();
+		};
+	}, []);
+
+	useEffect(() => {
+		if (!localPlayer) return;
+		async function connect() {
+			await localPlayer.connect();
+		}
+
+		connect();
+		return () => {
+			localPlayer.disconnect();
+		};
+	}, [localPlayer]);
+
+	useEffect(() => {
+		const transferMyPlayback = async () => {
+			if (device) {
+				await spotifyApi.transferMyPlayback([device], true);
+			}
+		};
+		const getDeviceFromApi = async () => {
+			await spotifyApi.getMyDevices();
+		};
+		getDeviceFromApi();
+		transferMyPlayback();
+	}, [device, spotifyApi]);
+
+	console.log({ current_track });
+
 	return (
 		<Box>
 			<Grid
@@ -26,14 +101,16 @@ const Player = ({ spotifyApi }) => {
 					}}
 				>
 					<Avatar
-						src={'https://upload.wikimedia.org/wikipedia/en/9/90/Scorpion_by_Drake.jpg'}
+						src={current_track?.album.images[0].url}
 						alt={'#'}
 						variant="square"
 						sx={{ width: 56, height: 56, marginRight: 2 }}
 					/>
 					<Box>
-						<Typography sx={{ color: 'text.primary', fontSize: 14 }}>Can't take a joke</Typography>
-						<Typography sx={{ color: 'text.secondary', fontSize: 12 }}>Drake</Typography>
+						<Typography sx={{ color: 'text.primary', fontSize: 14 }}>{current_track?.name}</Typography>
+						<Typography sx={{ color: 'text.secondary', fontSize: 12 }}>
+							{current_track?.artists[0].name}
+						</Typography>
 					</Box>
 				</Grid>
 				<Grid
