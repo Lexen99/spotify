900df79d6c8eeff7e40ad20168d34a3a01f4c57b
Marcus Pousette <marcuspousette@gmail.com>
2023-03-23 09:53:39 +0100

    Lektion 6 - Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css';
 import { Box } from '@mui/material';
-import Login from './pages/Login.jsx';
+import Home from './pages/Home.jsx';
 
 function App({ spotifyApi }) {
 	return (
 		<Box className="App">
-			<Login />
+			<Home />
 		</Box>
 	);
 }
