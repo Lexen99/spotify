f55168f90b681669fef69afb952b6085db838eed
Marcus Pousette <marcuspousette@gmail.com>
2023-03-23 09:50:37 +0100

    Lektion 5 - Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import './App.css';
-import { Box, Button, Stack } from '@mui/material';
+import { Box } from '@mui/material';
+import Login from './pages/Login.jsx';
 
 function App({ spotifyApi }) {
 	return (
 		<Box className="App">
-			<h1>Techover Self Made - Spotify</h1>
-			<Stack spacing={2} direction="row">
-				<Button variant="text">Text</Button>
-				<Button variant="contained">Contained</Button>
-				<Button variant="outlined">Outlined</Button>
-			</Stack>
+			<Login />
 		</Box>
 	);
 }
