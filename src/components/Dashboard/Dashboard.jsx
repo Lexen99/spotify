c0841d7ce86e2d228dbc19cbe4009128a7fdf80d
Marcus Pousette <marcuspousette@gmail.com>
2023-03-23 13:50:03 +0100

    Lektion 10 - SideNav

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import { Box } from '@mui/material';
 import { Routes, Route } from 'react-router-dom';
 import Home from '../../pages/Home';
+import SideNav from '../SideNav/SideNav';
+import { getAccessTokenFromStorage } from '../../utils/getAccessTokenFromStorage';
 
 const Dashboard = ({ spotifyApi }) => {
+	const token = useState(getAccessTokenFromStorage());
+
+	useEffect(() => {
+		const onMount = async () => {
+			await spotifyApi.setAccessToken(token);
+		};
+
+		if (token) {
+			onMount();
+		}
+	}, []);
+
 	return (
 		<Box
 			sx={{
@@ -14,6 +28,7 @@ const Dashboard = ({ spotifyApi }) => {
 			}}
 		>
 			<Box sx={{ flex: 1, overflowY: 'auto', display: 'flex' }}>
+				<SideNav spotifyApi={spotifyApi} token={token} />
 				<Routes>
 					<Route path="/playlist/:id" element={<div>Playlist</div>} />
 					<Route path="/library" element={<div>Library</div>} />
---
+++ src/components/SideNav/SideNav.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+import { Box, Divider } from '@mui/material';
+
+const SideNav = ({ spotifyApi, token }) => {
+	const [albumList, setAlbumList] = useState(null);
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		async function getPlaylists() {
+			if (!spotifyApi) return;
+
+			const data = await spotifyApi.getUserPlaylists();
+			console.log(data);
+
+			setLoading(false);
+			setAlbumList(data.body.items);
+		}
+		getPlaylists();
+	}, [spotifyApi, token]);
+
+	return (
+		<Box
+			sx={{
+				bgcolor: 'background.default',
+				width: 230,
+				height: '100%',
+				display: { xs: 'none', md: 'flex' },
+				flexDirection: 'column'
+			}}
+		>
+			<Box p={3}>
+				<img src="/Spotify_Logo.png" width={'75%'} alt="Spotify" />
+			</Box>
+			<Box px={3} py={1}>
+				<Divider sx={{ backgroundColor: '#ffffff40' }} />
+			</Box>
+			<Box sx={{ overflowY: 'auto', flex: 1 }}>{/* Playlists here */}</Box>
+		</Box>
+	);
+};
+
+export default SideNav;
