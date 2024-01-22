ec379757417453c315f1c5a5742d096f9dc9e678
Oliver Lexenberg <o.lexenberg@hotmail.com>
2024-01-22 10:35:56 +0100

    Added logic to get access-token, and the Route system

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
 import './App.css';
 import { Box } from '@mui/material';
+import Dashboard from './components/Dashboard/Dashboard';
 import Login from './pages/Login';
-import Home from './pages/Home'
+import { getAccessToken } from './utils/getAccessToken';
+import { getAccessTokenFromStorage } from './utils/getAccessTokenFromStorage';
+import { useEffect, useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 function App({ spotifyApi }) {
+	const [token, setToken] = useState(getAccessTokenFromStorage());
+
+	useEffect(() => {
+		const AccessToken = getAccessTokenFromStorage() || getAccessToken();
+
+		if (AccessToken) {
+			setToken(AccessToken);
+			sessionStorage.setItem('spotifyToken', AccessToken);
+			window.location.hash = '';
+		}
+	}, []);
+
 	return (
 		<Box className="App">
-			<Home />
+			{token ? (
+				<Dashboard spotifyApi={spotifyApi} />
+			) : (
+				<Routes>
+					<Route path="*" element={<Login />} />
+				</Routes>
+			)}
 		</Box>
 	);
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,21 +3,23 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import SpotifyWebApi from 'spotify-web-api-node';
-import {redirectURL} from './config/config';
-
+import { redirectURL } from './config/config';
 import { ThemeProvider } from '@mui/system';
 import { themeOptions } from './theme/material-theme';
+import { BrowserRouter } from 'react-router-dom';
 
 const spotifyApi = new SpotifyWebApi({
-  clientId: import.meta.env.VITE_CLIENT_ID,
-  clientSecret: import.meta.env.VITE_CLIENT_SECRET,
-  redirectUri: redirectURL
+	clientId: import.meta.env.VITE_CLIENT_ID,
+	clientSecret: import.meta.env.VITE_CLIENT_SECRET,
+	redirectUri: redirectURL
 });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<ThemeProvider theme={themeOptions}>
-			<App spotifyApi={spotifyApi}/>
-		</ThemeProvider>
+		<BrowserRouter>
+			<ThemeProvider theme={themeOptions}>
+				<App spotifyApi={spotifyApi} />
+			</ThemeProvider>
+		</BrowserRouter>
 	</React.StrictMode>
 );
